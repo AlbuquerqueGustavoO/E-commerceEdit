cc964e41afe432eeeacc0e03e2e849acc56846a4
Gustavo Albuquerque <gustavoo_albuquerque@hotmail.com>
2020-04-22 18:58:55 -0300

    Deploy aplicação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpModule } from '@angular/http';
 import { RouterModule, PreloadAllModules } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { LocationStrategy, HashLocationStrategy } from '@angular/common'
 
 
 import { ROUTES } from './app.routes'
@@ -49,7 +50,7 @@ import { NotFoundComponent } from './not-found/not-found.component';
     SheredModule.forRoot(),
     RouterModule.forRoot(ROUTES, { preloadingStrategy: PreloadAllModules })
   ],
-  providers: [{ provide: LOCALE_ID, useValue: 'pt-BR' }],
+  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }, { provide: LOCALE_ID, useValue: 'pt-BR' }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
